791dddf89c8b4a3b83f35df87ac3d367e791139f
di <dmitry.vasilevskiy@gmail.com>
2023-02-05 22:55:11 +0300

    fix & db:seed

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -6,13 +6,14 @@ use App\Http\Requests\ItemStoreRequest;
 use App\Models\Item;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
     public function store(ItemStoreRequest $request)
     {
         try {
-            return Item::query()->create(['name' => $request->all()['name']]);
+            return Item::query()->create(['name' => $request->all()['name'], 'user_id', Auth::id()]);
         } catch (\Exception $e) {
             return response(['message' => $e->getMessage()], 500);
         }
